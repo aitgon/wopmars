1dbd9b2cc1e8c7a400ebed2b93986e3424e7fe4d
luc <luc.giffo@gmail.com>
2016-06-03 14:10:42 +0200

    logs colorés

--- src/main/fr/tagc/wopmars/utils/Logger.py
+++ src/main/fr/tagc/wopmars/utils/Logger.py
@@ -2,9 +2,12 @@
 Module containing the Logger class.
 """
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
+from fr.tagc.wopmars.utils.ColorPrint import ColorPrint
 from fr.tagc.wopmars.utils.OptionManager import OptionManager
+from fr.tagc.wopmars.utils.PathFinder import PathFinder
 from fr.tagc.wopmars.utils.Singleton import singleton
 
 
@@ -26,54 +29,64 @@ class Logger:
         formatter_file = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 
         # file in append mode of size 1 Mo and 1 backup
-        file_handler = RotatingFileHandler('/home/giffon/wopmars.log', 'a', 1000000, 1)
+        file_handler = RotatingFileHandler(PathFinder.find_src(os.path.dirname(os.path.realpath(__file__))) + '../log/wopmars.log', 'a', 1000000, 1)
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter_file)
 
         # stream ------------------:
-
-        # todo ask aitor afficher de la couleur dans la console? oui
-        formatter_stream = logging.Formatter('%(levelname)s :: %(message)s')
-
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter_stream)
+        self.__stream_handler = logging.StreamHandler()
 
         verbosity = OptionManager()["-v"]
 
         if verbosity == 1:
-            stream_handler.setLevel(logging.ERROR)
+            self.__stream_handler.setLevel(logging.ERROR)
         elif verbosity == 2:
-            stream_handler.setLevel(logging.WARNING)
-        elif verbosity == 3 or stream_handler == 0:
-            stream_handler.setLevel(logging.INFO)
+            self.__stream_handler.setLevel(logging.WARNING)
+        elif verbosity == 3 or verbosity == 0:
+            self.__stream_handler.setLevel(logging.INFO)
         elif verbosity == 4:
-            stream_handler.setLevel(logging.DEBUG)
+            self.__stream_handler.setLevel(logging.DEBUG)
 
-        # configure logger -------------------:
+        # configure loggers -------------------:
 
         self.__logger = logging.getLogger()
         self.__logger.setLevel(logging.DEBUG)
-
         self.__logger.addHandler(file_handler)
-        self.__logger.addHandler(stream_handler)
+        self.__logger.addHandler(self.__stream_handler)
 
     def info(self, msg):
+        formatter_stream = logging.Formatter(ColorPrint.blue('%(levelname)s :: %(message)s'))
+        self.__stream_handler.setFormatter(formatter_stream)
+
         self.__logger.info(msg)
 
     def debug(self, msg):
+        formatter_stream = logging.Formatter(ColorPrint.yellow('%(levelname)s :: %(message)s'))
+        self.__stream_handler.setFormatter(formatter_stream)
+
         self.__logger.debug(msg)
 
     def error(self, msg):
+        formatter_stream = logging.Formatter(ColorPrint.red('%(levelname)s :: %(message)s'))
+        self.__stream_handler.setFormatter(formatter_stream)
+
         self.__logger.error(msg)
 
     def warning(self, msg):
+        formatter_stream = logging.Formatter(ColorPrint.red('%(levelname)s :: %(message)s'))
+        self.__stream_handler.setFormatter(formatter_stream)
+
         self.__logger.warning(msg)
 
     def critical(self, msg):
+        formatter_stream = logging.Formatter(ColorPrint.red('%(levelname)s :: %(message)s'))
+        self.__stream_handler.setFormatter(formatter_stream)
+
         self.__logger.critical(msg)
 
 
 if __name__ == "__main__":
+    OptionManager()["-v"] = 4
     l = Logger()
     l.info("salut")
     l.debug("salut")
