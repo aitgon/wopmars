2abcb1fe9011f14a3b026e010b2c19f6f991e70c
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-04-26 06:30:02 +0200

    CLN Reset versions

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-__version__='1.1.30'
+__version__='0.0.1'
 
 here = path.abspath(path.dirname(__file__))
 
